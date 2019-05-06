0b074ed8787d350023a09196fa90e27a7c5e24ba
Evan Poworoznek <poworoznek@comcast.net>
2019-05-05 20:42:19 -0400

    CAPout / PSFout / assigner

---
+++ applier.R
@@ -0,0 +1,10 @@
+# Apply CAPout output to a list
+
+# ARGUMENTS: l: list of vectors to apply to permutations to
+#            p: permutation / sign output from  CAPout
+
+applier = function(l, p){
+  return(mapply(function(vec, perm){
+    return(vec[abs(perm)] * sign(perm))
+  }, l, p, SIMPLIFY = FALSE))
+}
