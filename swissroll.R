bdcc5684bf5f8caa8616acbed122869b2a31d6a8
Evan Poworoznek <poworoznek@comcast.net>
2019-10-21 13:44:55 -0400

    swiss roll

---
+++ swissroll.R
@@ -0,0 +1,21 @@
+# swiss roll ex
+library(plot3D)
+
+phi = runif(1000, 1.5 * pi, 4.5 * pi)
+psi = runif(1000, 1, 10)
+
+x = phi * cos(phi)
+y = phi * sin(phi)
+z = psi
+
+points3D(x, y, z, colvar = phi)
+
+kern = function(X, Y, c){
+  return(exp(-c*sum((X-Y)^2)))
+}
+
+dat = cbind(x, y, z)
+
+k = apply(dat, 1, function(X){
+  apply(dat, 1, kern, X, 0.1)
+})
